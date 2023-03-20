4d3641c34a696e1fc513cd3bddc57f557de10240
仑质 <benzenpenxil@foxmail.com>
2023-03-20 10:54:15 +0800

    版本推进至3.0.13，新增说明

--- OlivaDiceNativeGUI/data.py
+++ OlivaDiceNativeGUI/data.py
@@ -15,8 +15,8 @@ _  / / /_  /  __  / __ | / /__  /| |_  / / /__  / _  /    __  __/
 '''
 
 
-OlivaDiceNativeGUI_ver = '3.0.12'
-OlivaDiceNativeGUI_svn = 13
+OlivaDiceNativeGUI_ver = '3.0.13'
+OlivaDiceNativeGUI_svn = 14
 OlivaDiceNativeGUI_ver_short = '%s(%s)' % (str(OlivaDiceNativeGUI_ver), str(OlivaDiceNativeGUI_svn))
 
 onlineStatusAPIURL = 'https://api.oliva.icu/info/'
